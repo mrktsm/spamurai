d46746172d15c2a77aa63fb93eefcb6867e8ed6d
Marko Tsymbaliuk <markotsymbaluk@gmail.com>
2024-12-03 09:01:40 -0500

    Added AOuth modal and its logic

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,4 +4,15 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: "index.html", // Include the main entry point
+        modal: "modal.html", // The modal HTML
+      },
+      output: {
+        entryFileNames: `assets/[name].js`,
+      },
+    },
+  },
 });
