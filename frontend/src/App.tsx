b8bd9ef0c816a69afb543b53501251995bf06117
Marko Tsymbaluk <markotsymbaluk@gmail.com>
2025-01-05 11:16:35 -0500

    Added a GitHub button to the header

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub } from "@fortawesome/free-brands-svg-icons"; // Import the GitHub icon
 import Switch from "./components/Switch";
-// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-// import { faGear } from "@fortawesome/free-solid-svg-icons";
 
 function App() {
   const [definitionEnabled, setDefinitionEnabled] = useState(true);
@@ -17,8 +17,18 @@ function App() {
               Spamurai
             </h1>
           </div>
+          {/* GitHub Icon in the header, aligned to the right */}
+          <a
+            href="https://github.com/mrktsm/spamurai"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-rose-300 transform transition-all duration-300 hover:text-rose-500 hover:scale-105"
+          >
+            <FontAwesomeIcon icon={faGithub} className="w-5 h-5 mr-2" />
+          </a>
         </header>
       </div>
+
       <div className="px-2">
         <div className="p-3 w-80">
           <h2 className="text-sm font-bold text-left mb-4 text-rose-100">
@@ -28,9 +38,7 @@ function App() {
           {/* Display Button Setting */}
           <div className="mb-4">
             <div className="flex justify-between items-center">
-              <span className="text-sm text-gray-100">
-                Auto Popup for Spam Emails
-              </span>
+              <span className="text-sm text-gray-100">Protection Mode</span>
               <div className="ml-auto">
                 <Switch
                   isOn={definitionEnabled}
@@ -39,7 +47,8 @@ function App() {
               </div>
             </div>
             <p className="text-xs text-gray-400 mt-2">
-              Opens the popup automatically when spam is detected in an email.
+              Enable or disable Spamurai's advanced spam filtering for your
+              emails.
             </p>
             <hr className="my-3 border-gray-700" />
           </div>
@@ -47,7 +56,9 @@ function App() {
           {/* Play Sound Setting */}
           <div>
             <div className="flex justify-between items-center">
-              <span className="text-sm text-gray-100">Protection Mode</span>
+              <span className="text-sm text-gray-100">
+                Auto Popup for Spam Emails{" "}
+              </span>
               <div className="ml-auto">
                 <Switch
                   isOn={playSoundEnabled}
@@ -56,8 +67,7 @@ function App() {
               </div>
             </div>
             <p className="text-xs text-gray-400 mt-2">
-              Enable or disable Spamurai's advanced spam filtering for your
-              emails.
+              Opens the popup automatically when spam is detected in an email.
             </p>
           </div>
         </div>
