bcc65e089db9f47ee891ca04500cd25fe975899b
Marko Tsymbaluk <markotsymbaluk@gmail.com>
2025-01-05 10:53:44 -0500

    Added some settings and their description to the Pop up

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -13,7 +13,7 @@ function App() {
       <div className="w-full">
         <header className="bg-zinc-900 flex justify-between items-center p-3">
           <div className="flex items-center">
-            <h1 className="text-sm text-left font-medium text-gray-100 ml-2">
+            <h1 className="text-sm text-left font-semibold text-rose-300 ml-2">
               Spamurai
             </h1>
           </div>
@@ -21,14 +21,16 @@ function App() {
       </div>
       <div className="px-2">
         <div className="p-3 w-80">
-          <h2 className="text-sm font-bold text-left mb-4 text-gray-100">
+          <h2 className="text-sm font-bold text-left mb-4 text-rose-100">
             Quick settings
           </h2>
 
           {/* Display Button Setting */}
           <div className="mb-4">
             <div className="flex justify-between items-center">
-              <span className="text-sm text-gray-100">Settings 1</span>
+              <span className="text-sm text-gray-100">
+                Auto Popup for Spam Emails
+              </span>
               <div className="ml-auto">
                 <Switch
                   isOn={definitionEnabled}
@@ -36,14 +38,16 @@ function App() {
                 />
               </div>
             </div>
-            <p className="text-xs text-gray-400 mt-2">Settings 1 description</p>
+            <p className="text-xs text-gray-400 mt-2">
+              Opens the popup automatically when spam is detected in an email.
+            </p>
             <hr className="my-3 border-gray-700" />
           </div>
 
           {/* Play Sound Setting */}
           <div>
             <div className="flex justify-between items-center">
-              <span className="text-sm text-gray-100">Setting 2</span>
+              <span className="text-sm text-gray-100">Protection Mode</span>
               <div className="ml-auto">
                 <Switch
                   isOn={playSoundEnabled}
@@ -51,7 +55,10 @@ function App() {
                 />
               </div>
             </div>
-            <p className="text-xs text-gray-400 mt-2">Settings 2 description</p>
+            <p className="text-xs text-gray-400 mt-2">
+              Enable or disable Spamurai's advanced spam filtering for your
+              emails.
+            </p>
           </div>
         </div>
       </div>
